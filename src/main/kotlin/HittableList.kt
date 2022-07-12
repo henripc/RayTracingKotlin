7929fc737599d76fd1804d87ef4c136effc19092
henripc <henriquechimetta@outlook.com>
2022-07-12 09:10:05 -0300

    Create HittableList class.
    Added a class that represents a list of hittable objects.

---
+++ src/main/kotlin/HittableList.kt
@@ -0,0 +1,29 @@
+class HittableList() : Hittable {
+    private val objects: MutableList<Hittable> = ArrayList()
+
+    constructor(obj: Hittable) : this() {
+        this.add(obj)
+    }
+
+    fun clear() = this.objects.clear()
+    fun add(obj: Hittable) = this.objects.add(obj)
+
+    override fun hit(r: Ray, tMin: Double, tMax: Double, rec: HitRecord): Boolean {
+        val tempRec = HitRecord()
+        var hitAnything = false
+        var closestSoFar = tMax
+
+        for (obj in this.objects) {
+            if (obj.hit(r, tMin, closestSoFar, tempRec)) {
+                hitAnything = true
+                closestSoFar = tempRec.t
+                rec.p = tempRec.p
+                rec.normal = tempRec.normal
+                rec.t = tempRec.t
+                rec.frontFace = tempRec.frontFace
+            }
+        }
+
+        return hitAnything
+    }
+}
