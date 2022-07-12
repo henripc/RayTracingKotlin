be81d9427b189649699160a34bb5064929c1dcef
henripc <henriquechimetta@outlook.com>
2022-07-12 09:09:04 -0300

    Create RtWeekend class.
    Utility class.

---
+++ src/main/kotlin/RtWeekend.kt
@@ -0,0 +1,10 @@
+class RtWeekend {
+    companion object {
+        // Constants
+        const val INFINITY = Double.POSITIVE_INFINITY
+        const val PI = 3.141592653589793
+
+        // Utility Functions
+        fun degreesToRadians(degrees: Double) = degrees * PI / 180
+    }
+}
